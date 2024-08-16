776cd9234a076b38ff42c97986f780e24f1abcd8
Deva Ahmad <devaahmad43@gmail.com>
2024-08-16 10:33:44 +0700

    membuat animasi navbar mobile 4

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(document).ready(()=>{
     
+    let hamburger = $('.hbg-menu')
+    let ham_click = ()=>{
+        let nav = $('.navigation-menu').toggleClass('navi-anim')
+    }
+
+    hamburger.on('click', ham_click)
 })
