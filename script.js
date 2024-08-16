8975b08235a263066daba41c040ca9533b5178f3
Deva Ahmad <devaahmad43@gmail.com>
2024-08-16 14:29:04 +0700

    membuat darkmode 3

--- script.js
+++ script.js
@@ -6,4 +6,26 @@ $(document).ready(()=>{
     }
 
     hamburger.on('click', ham_click)
+
+    let theme_switch = $("#theme-switch")
+    let darkmode = localStorage.getItem('dark-mode')
+
+    let  enable_darkmode =() =>{
+        document.body.classList.add('dark-mode')
+        localStorage.setItem('dark-mode', 'active')
+    }
+
+    let  disable_darkmode =() =>{
+        document.body.classList.remove('dark-mode')
+        localStorage.setItem('dark-mode', null)
+    }
+
+    if (darkmode === "active") enable_darkmode()
+
+    let darkmode_click = () =>{
+        darkmode = localStorage.getItem('dark-mode')
+        darkmode !== "active" ? enable_darkmode() : disable_darkmode()
+    }
+
+    theme_switch.on('click', darkmode_click)
 })
